b08eee272902e774c7119961273a72348a278da6
Serhii <s.shkurenko@treeum.net>
2025-08-10 12:39:57 +0300

    add skip login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,6 +3,8 @@ import {baseConfig} from "../config/baseConfig";
 import {ApplicationPage} from "../pages/appPage";
 
 test.describe('Login', () => {
+    test.skip(!!process.env.CI, 'Test is skipped in CI due to the Cloudflare protection.');
+
     test('Verify login with valid credentials', async ({ page }) => {
         const app = new ApplicationPage(page);
 
