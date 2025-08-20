09d9184912c42a5411e4d77c4c2fb8d2c469a7f1
Serhii <s.shkurenko@treeum.net>
2025-08-20 13:04:48 +0300

    update

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -7,8 +7,6 @@ export class LoginPage extends BasePage{
     readonly loginButton: Locator = this.page.getByRole('button', {name: 'Login'});
 
     async loginAs(email: string, password: string) {
-        await this.page.waitForURL('**/auth/login', { timeout: 15_000 });
-        await this.emailField.waitFor({ state: 'visible' });
         await this.emailField.fill(email);
         await this.passwordField.fill(password);
         await this.loginButton.click();
--- tests/auth-setup.spec.ts
+++ tests/auth-setup.spec.ts
@@ -1,17 +1,14 @@
 import {expect, test} from "@playwright/test";
 import { USER_EMAIL, USER_PASSWORD } from '../config/baseConfig';
-import {Application} from "../pages/app";
 import path from "path";
+
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-test.skip(!!process.env.CI, 'Test is skipped in CI due to the Cloudflare protection.');
+test('Verify successful login', async ({ request }) => {
+    const response = await request.post('/auth/login', {
+        data: { email: USER_EMAIL, password: USER_PASSWORD },
+    });
+    expect(response.status()).toBe(200);
 
-test('Verify successful login', async ({ page }) => {
-    const app = new Application(page);
-    await app.login.navigateTo('/auth/login');
-    await app.login.loginAs(USER_EMAIL, USER_PASSWORD)
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
-
-    await page.context().storageState({ path: authFile });
+    await request.storageState({ path: authFile });
 });
-
