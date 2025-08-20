121369ea9549ac80e4c96641ac4b1f9f9ad75e86
Serhii <s.shkurenko@treeum.net>
2025-08-20 14:59:32 +0300

    update playwright.yml and baseConfig.ts

--- config/baseConfig.ts
+++ config/baseConfig.ts
@@ -1,7 +1,6 @@
 import { config as dotenvConfig } from 'dotenv';
-import { join } from 'path';
 
-dotenvConfig({ path: join(__dirname, '..', '.env') });
+dotenvConfig();
 
 export const WEB_URL: string = process.env.WEB_URL!;
 export const USER_EMAIL: string = process.env.USER_EMAIL!;
