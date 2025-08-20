1aa0744e81220ec0a48280c0e56b1eb63daeeb90
Serhii <s.shkurenko@treeum.net>
2025-08-20 21:23:14 +0300

    update auth-setup.spec.ts

--- tests/auth-setup.spec.ts
+++ tests/auth-setup.spec.ts
@@ -1,14 +1,30 @@
-import {expect, test} from "@playwright/test";
-import { USER_EMAIL, USER_PASSWORD } from '../config/baseConfig';
+import {USER_EMAIL, USER_PASSWORD, WEB_URL} from '../config/baseConfig';
 import path from "path";
+import {expect, test as setup} from '@playwright/test';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-test('Verify successful login', async ({ request }) => {
-    const response = await request.post('/auth/login', {
-        data: { email: USER_EMAIL, password: USER_PASSWORD },
+setup('Authenticate and save token', async ({ browser, request }) => {
+    const res = await request.post('https://api.practicesoftwaretesting.com/users/login', {
+        data: {
+            email: USER_EMAIL,
+            password: USER_PASSWORD,
+        },
     });
-    expect(response.status()).toBe(200);
 
-    await request.storageState({ path: authFile });
+    expect(res.status()).toBe(200);
+
+    const body = await res.json();
+    const token = body?.access_token || body?.token;
+    expect(token).toBeTruthy();
+
+    const page = await browser.newPage();
+    await page.goto(WEB_URL);
+
+    await page.evaluate((token) => {
+        localStorage.setItem('auth-token', token);
+    }, token);
+
+    await page.context().storageState({ path: authFile });
+    await page.close();
 });
