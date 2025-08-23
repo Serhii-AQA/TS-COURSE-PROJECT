2fb847abc17604fabe4d0f9890193e7941ddf0c4
Serhii <s.shkurenko@treeum.net>
2025-08-23 11:28:12 +0300

    Update apiUtils.ts

--- utils/apiUtils.ts
+++ utils/apiUtils.ts
@@ -1,31 +1,6 @@
 import { expect, APIRequestContext } from "@playwright/test";
 import {Url} from "../constants/url";
 
-// export async function waitForApiStatus(request: APIRequestContext, url: string, expectedStatus: number = 200) {
-//     await expect.poll(async () => {
-//         const response = await request.get(url);
-//         return response.status();
-//     }, {
-//         message: `API ${url} don't returned status ${expectedStatus}`,
-//     }).toBe(expectedStatus);
-// }
-
-
-// export async function waitForApiStatus(request: APIRequestContext, url: string, expectedStatus: number = 200) {
-//     await expect.poll(async () => {
-//         const response = await request.get(url, {
-//             headers: {
-//                 Authorization: `Bearer ${process.env.API_TOKEN}`,
-//             },
-//         });
-//         return response.status();
-//     }, {
-//         message: `API ${url} didn't return status ${expectedStatus}`,
-//         timeout: 15000,
-//     }).toBe(expectedStatus);
-// }
-
-
 export async function waitForApiStatus(request: APIRequestContext, endpoint: string, expectedStatus: number = 200) {
     const url = `${Url.ApiBase}${endpoint}`;
     console.log('url -------> ', url)
@@ -37,4 +12,3 @@ export async function waitForApiStatus(request: APIRequestContext, endpoint: str
         timeout: 15000,
     }).toBe(expectedStatus);
 }
-
