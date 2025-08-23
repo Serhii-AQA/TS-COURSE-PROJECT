c96d6f9da893c064287c4c0b68b06df33105ebbb
Serhii <s.shkurenko@treeum.net>
2025-08-23 11:03:56 +0300

    Update apiUtils.ts

--- utils/apiUtils.ts
+++ utils/apiUtils.ts
@@ -10,11 +10,28 @@ import { expect, APIRequestContext } from "@playwright/test";
 // }
 
 
-export async function waitForApiStatus(request: APIRequestContext, url: string, expectedStatus: number = 200) {
+// export async function waitForApiStatus(request: APIRequestContext, url: string, expectedStatus: number = 200) {
+//     await expect.poll(async () => {
+//         const response = await request.get(url, {
+//             headers: {
+//                 Authorization: `Bearer ${process.env.API_TOKEN}`,
+//             },
+//         });
+//         return response.status();
+//     }, {
+//         message: `API ${url} didn't return status ${expectedStatus}`,
+//         timeout: 15000,
+//     }).toBe(expectedStatus);
+// }
+
+
+export async function waitForApiStatus(request: APIRequestContext, endpoint: string, expectedStatus: number = 200) {
+    const url = `${process.env.WEB_URL}${endpoint}`;
     await expect.poll(async () => {
         const response = await request.get(url, {
             headers: {
-                Authorization: `Bearer ${process.env.API_TOKEN}`,
+                // якщо потрібна авторизація
+                // Authorization: `Bearer ${process.env.API_TOKEN}`,
             },
         });
         return response.status();
@@ -23,3 +40,4 @@ export async function waitForApiStatus(request: APIRequestContext, url: string, 
         timeout: 15000,
     }).toBe(expectedStatus);
 }
+
