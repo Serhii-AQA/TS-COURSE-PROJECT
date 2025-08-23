dc7d663dc960eeb15a5786e78b7c6fbf71d7b8af
Serhii <s.shkurenko@treeum.net>
2025-08-23 11:28:36 +0300

    Update apiUtils.ts

--- utils/apiUtils.ts
+++ utils/apiUtils.ts
@@ -3,12 +3,11 @@ import {Url} from "../constants/url";
 
 export async function waitForApiStatus(request: APIRequestContext, endpoint: string, expectedStatus: number = 200) {
     const url = `${Url.ApiBase}${endpoint}`;
-    console.log('url -------> ', url)
     await expect.poll(async () => {
         const response = await request.get(url);
         return response.status();
     }, {
         message: `API ${url} didn't return status ${expectedStatus}`,
-        timeout: 15000,
+        timeout: 10_000,
     }).toBe(expectedStatus);
 }
